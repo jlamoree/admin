6c94b778b684d420ba7861fc81a354d6e51de6e0
jlamoree <joseph@lamoree.com>
2010-06-16 17:48:24 +0000

    Added option to perform a complete update and skip download if file already exists.
    git-svn-id: https://svn.lamoree.com/svn/admin@40 1b74ea4a-2c61-0410-8925-a3c94e0a55a1

--- coldfusion/openbluedragon-nightly-updater.sh
+++ coldfusion/openbluedragon-nightly-updater.sh
@@ -10,14 +10,67 @@ SELF="`basename $0 | sed -E 's/\.[a-z]+\$//'`"
 WORK="`mktemp -d /tmp/$SELF.XXXX`"
 DOWNLOAD="$WORK/$NIGHTLY"
 UA="$SELF/0.01"
+FILE="_"
+MINIMAL="no"
 
-curl -'#' -A "$UA" -o "$DOWNLOAD" $URL
-test -f "$DOWNLOAD" || exit 1
+function help {
+  echo "Usage: $SELF [options]"
+  echo "Options:"
+  echo "  -h : Help (this message)."
+  echo "  -m : Minimal update -- just OpenBlueDragon.jar"
+  echo "  -f : File to use, rather than downloading"
+  echo 
+}
 
-mv "$DOWNLOAD" "$ARCHIVE"
+function error {
+  echo "Error: $1"
+  exit 1
+}
+
+while getopts ":hmf:" FLAG; do
+  case $FLAG in
+    "h")
+       help 
+       exit 0
+    ;;
+    "m")
+       MINIMAL="yes"
+    ;;
+    "f")
+       FILE="$OPTARG"
+    ;;
+    "?")
+       error "Invalid option '$OPTARG'"
+       exit 1
+    ;;
+  esac
+done
+
+if [ "$FILE" == "_" ]; then
+  curl -'#' -A "$UA" -o "$DOWNLOAD" $URL
+  if [ ! -f "$DOWNLOAD" ]; then
+    error "The file was not downloaded successfully."
+  fi
+  mv "$DOWNLOAD" "$ARCHIVE"
+elif [ ! -f "$FILE" ]; then
+  error "The file specified ($FILE) is not accessible."
+else
+  ARCHIVE="$FILE"
+fi
+
 md5 -r "$ARCHIVE"
-unzip -qq -j -d "$WORK" "$ARCHIVE" WEB-INF/lib/OpenBlueDragon.jar
-unzip -qq -c "$WORK/OpenBlueDragon.jar" openbd.properties
-mv -f "$WORK/OpenBlueDragon.jar" "$DEPLOY/$CONTEXT/WEB-INF/lib/OpenBlueDragon.jar"
+if [ "$MINIMAL" == "yes" ]; then
+  unzip -qq -j -d "$WORK" "$ARCHIVE" WEB-INF/lib/OpenBlueDragon.jar
+	unzip -qq -c "$WORK/OpenBlueDragon.jar" openbd.properties
+	mv -f "$WORK/OpenBlueDragon.jar" "$DEPLOY/$CONTEXT/WEB-INF/lib/OpenBlueDragon.jar"
+  echo "Finished a minimal update by replacing the OpenBlueDragon.jar file."
+else
+  unzip -qq -d "$WORK" "$ARCHIVE"
+  for D in bluedragon demo manual WEB-INF/lib; do
+    rm -rf "$DEPLOY/$CONTEXT/$D"
+    mv -f "$WORK/$D" "$DEPLOY/$CONTEXT/$D"
+  done
+  echo "Finished a complete update of Open BlueDragon."
+fi
 rm -rf "$WORK"
 exit 0
